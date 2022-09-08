68318f456fbc0b13b1fcf2c1c1fe115f7d9d92b7
ZumiSolutions <zumiapplication2020@gmail.com>
2022-09-08 16:55:50 +0530

    MainActivity2

--- app/src/main/java/com/poc/chat_application/MainActivity.java
+++ app/src/main/java/com/poc/chat_application/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "noohu1", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "noohu2", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "noohu8", Toast.LENGTH_LONG).show();
     }
 }
