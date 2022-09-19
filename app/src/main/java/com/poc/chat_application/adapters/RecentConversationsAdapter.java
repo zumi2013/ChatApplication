1f5d8f2552b44c54fcf1e24081f43493db840ee8
ZumiSolutions <zumiapplication2020@gmail.com>
2022-09-19 11:12:22 +0530

    Chat Send and receive1

---
+++ app/src/main/java/com/poc/chat_application/adapters/RecentConversationsAdapter.java
@@ -0,0 +1,79 @@
+package com.poc.chat_application.adapters;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.poc.chat_application.databinding.ItemContainerRecentConversionBinding;
+import com.poc.chat_application.listeners.ConversationListner;
+import com.poc.chat_application.models.ChatMessage;
+import com.poc.chat_application.models.User;
+
+import java.util.List;
+
+public class RecentConversationsAdapter extends RecyclerView.Adapter<RecentConversationsAdapter.ConversionViewHolder> {
+
+    private final List<ChatMessage> chatMessages;
+    private final ConversationListner conversationListner;
+
+    public RecentConversationsAdapter(List<ChatMessage> chatMessages, ConversationListner conversationListner) {
+        this.chatMessages = chatMessages;
+        this.conversationListner = conversationListner;
+    }
+
+    @NonNull
+    @Override
+    public ConversionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ConversionViewHolder(ItemContainerRecentConversionBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ConversionViewHolder holder, int position) {
+        holder.setData(chatMessages.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    class ConversionViewHolder extends RecyclerView.ViewHolder {
+
+        ItemContainerRecentConversionBinding binding;
+
+        ConversionViewHolder(ItemContainerRecentConversionBinding itemContainerRecentConversionBinding) {
+            super(itemContainerRecentConversionBinding.getRoot());
+            binding = itemContainerRecentConversionBinding;
+
+        }
+
+        void setData(ChatMessage chatMessage) {
+            binding.imageProfile.setImageBitmap(getConversionImage(chatMessage.conversionImage));
+            binding.textName.setText(chatMessage.conversionName);
+            binding.textRecentMessage.setText(chatMessage.message);
+            binding.getRoot().setOnClickListener(V -> {
+                User user = new User();
+                user.id = chatMessage.conversionId;
+                user.name = chatMessage.conversionName;
+                user.image = chatMessage.conversionImage;
+                conversationListner.onConverstionClicked(user);
+            });
+        }
+    }
+
+    private Bitmap getConversionImage(String encodedImage) {
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+}
---
+++ app/src/main/java/com/poc/chat_application/listeners/ConversationListner.java
@@ -0,0 +1,7 @@
+package com.poc.chat_application.listeners;
+
+import com.poc.chat_application.models.User;
+
+public interface ConversationListner {
+    void onConverstionClicked(User user);
+}
